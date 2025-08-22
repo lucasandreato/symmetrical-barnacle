98031ae9effa74494b4b33881bb00db90a07906f
Lucas Andreato <lucasandreato@hotmail.com>
2025-08-22 01:14:37 -0300

    Create e2e.js

---
+++ cypress/support/e2e.js
@@ -0,0 +1,15 @@
+// ***********************************************************
+// This file is processed and loaded automatically before 
+// test files.
+// ***********************************************************
+
+import './commands'
+
+// Hide fetch/XHR requests from command log
+const app = window.top
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style')
+  style.innerHTML = '.command-name-request, .command-name-xhr { display: none }'
+  style.setAttribute('data-hide-command-log-request', '')
+  app.document.head.appendChild(style)
+}
